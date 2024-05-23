756879d1a4fe13b141e18cc74ec4ce795de031b1
zoe <zoe@zoedeMac-mini.local>
2024-05-23 18:37:02 +0800

    iOS 下载 demo 完成

---
+++ App.tsx
@@ -0,0 +1,156 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import RNFetchBlob from 'react-native-fs';
+import Share from 'react-native-share';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            <Button
+              onPress={() => {
+                request(PERMISSIONS.IOS.MEDIA_LIBRARY).then(stat => {
+                  console.log('stat', stat);
+                  if (stat === 'granted') {
+                    const savePath =
+                      RNFetchBlob.CachesDirectoryPath + '/name.pdf';
+                    const {promise: downPromise} = RNFetchBlob.downloadFile({
+                      fromUrl:
+                        'https://ebike-erp-1304383094.cos.ap-beijing.myqcloud.com/dev/H01/202405/01/171634735561374.png',
+                      toFile: savePath,
+                    });
+                    downPromise
+                      .then(() => {
+                        console.log('download success');
+
+                        if (Platform.OS === 'ios') {
+                          Share.open({
+                            saveToFiles: true,
+                            url: savePath,
+                          }).then(res => {
+                            if (res.success) console.log('share success');
+                          });
+                        } else if (Platform.OS === 'android') {
+                        }
+                      })
+                      .catch(error => {
+                        console.log('error', error);
+                      });
+                  }
+                });
+              }}
+              title="download"></Button>
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,120 @@
+# RN 下载 demo
+
+## iOS
+
+安装必要的包
+
+- react-native-fs 下载使用
+- react-native-permissions 获取权限
+- react-native-share 保存下载的内容到手机
+
+修改 podfile 文件
+
+```podfile
+# Resolve react_native_pods.rb with node to allow for hoisting
+# require Pod::Executable.execute_command('node', ['-p',
+#   'require.resolve(
+#     "react-native/scripts/react_native_pods.rb",
+#     {paths: [process.argv[1]]},
+#   )', __dir__]).strip
+def node_require(script)
+  # Resolve script with node to allow for hoisting
+  require Pod::Executable.execute_command('node', ['-p',
+    "require.resolve(
+      '#{script}',
+      {paths: [process.argv[1]]},
+    )", __dir__]).strip
+end
+
+  # Use it to require both react-native's and this package's scripts:
+node_require('react-native/scripts/react_native_pods.rb')
+node_require('react-native-permissions/scripts/setup.rb')
+
+
+
+platform :ios, min_ios_version_supported
+prepare_react_native_project!
+
+# ⬇️ uncomment the permissions you need
+setup_permissions([
+  # 'AppTrackingTransparency',
+  # 'Bluetooth',
+  # 'Calendars',
+  # 'CalendarsWriteOnly',
+  # 'Camera',
+  # 'Contacts',
+  # 'FaceID',
+  # 'LocationAccuracy',
+  # 'LocationAlways',
+  # 'LocationWhenInUse',
+  'MediaLibrary',
+  # 'Microphone',
+  # 'Motion',
+  # 'Notifications',
+  'PhotoLibrary',
+  'PhotoLibraryAddOnly',
+  # 'Reminders',
+  # 'Siri',
+  # 'SpeechRecognition',
+  # 'StoreKit',
+])
+
+linkage = ENV['USE_FRAMEWORKS']
+if linkage != nil
+  Pod::UI.puts "Configuring Pod with #{linkage}ally linked Frameworks".green
+  use_frameworks! :linkage => linkage.to_sym
+end
+
+target 'AwesomeProject1' do
+  config = use_native_modules!
+
+  use_react_native!(
+    :path => config[:reactNativePath],
+    # An absolute path to your application root.
+    :app_path => "#{Pod::Config.instance.installation_root}/.."
+  )
+
+  target 'AwesomeProject1Tests' do
+    inherit! :complete
+    # Pods for testing
+  end
+
+  post_install do |installer|
+    # https://github.com/facebook/react-native/blob/main/packages/react-native/scripts/react_native_pods.rb#L197-L202
+    react_native_post_install(
+      installer,
+      config[:reactNativePath],
+      :mac_catalyst_enabled => false,
+      # :ccache_enabled => true
+    )
+  end
+end
+
+```
+
+修改 `ios/AwesomeProject1/info.plist` 文件， 补充权限
+
+```.plist
+<dict>
+   // ....
+
+	<key>NSPhotoLibraryUsageDescription</key>
+	<string>使用相册</string>
+	<key>NSPhotoLibraryAddUsageDescription</key>
+	<string>使用相册 2</string>
+	<key>NSAppleMusicUsageDescription</key>
+	<string>使用音乐 2</string>
+</dict>
+```
+
+在 iOS 目录下执行 `pod install`
+
+重启项目即可
+
+###
+
+- xcode 缓存
+
+通常在这个目录，如果没有，可以在 xcode -> settings -> Locations -> Derived Data （下面有文件路径）
+
+`rm -rf ~/Library/Developer/Xcode/DerivedData`
